c1ab12b093186ef04174e9b9086e311ea2d37124
MrBigBrainy <mrbigbrainy@gmail.com>
2024-09-19 13:36:51 +0700

    Header

---
+++ component/Header.jsx
@@ -0,0 +1,16 @@
+export default function Header() {
+  return;
+  <header>
+    <nav>
+      <Hstack>
+        <ul>
+          <li>Home</li>
+          <li>Menu</li>
+          <li>Reservation</li>
+          <li>Order Online</li>
+          <li>Login</li>
+        </ul>
+      </Hstack>
+    </nav>
+  </header>;
+}
