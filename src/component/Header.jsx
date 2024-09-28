57f0d79f72cd39bbee2b886812ced31da331bb36
MrBigBrainy <mrbigbrainy@gmail.com>
2024-09-28 11:02:27 +0700

    Try to Responsive Header

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -4,8 +4,10 @@ import {
   Image,
   Box,
   Collapse,
-  Stack,
+  VStack,
   IconButton,
+  Flex,
+  Spacer,
 } from "@chakra-ui/react";
 import { HamburgerIcon, CloseIcon } from "@chakra-ui/icons";
 import { useDisclosure } from "@chakra-ui/react";
@@ -16,74 +18,12 @@ export default function Header() {
   return (
     <Box p={5} display={"flex"} justifyContent={"center"} as="header">
       <nav>
-        <ul style={{ listStyleType: "none" }}>
-          <HStack spacing={10} display={{ base: "none", md: "flex" }}>
+        <Flex justify={{ base: "space-between", md: "center" }} align="center">
+          <HStack spacing={10}>
             <Image src={logo} />
-            <Link
-              href="#"
-              fontFamily="Karla, sans-serif"
-              fontSize="lg"
-              fontWeight={"bold"}
-              color={"#495E57"}
-            >
-              Home
-            </Link>
-            <Link
-              href="#"
-              fontFamily="Karla, sans-serif"
-              fontSize="lg"
-              fontWeight={"bold"}
-              color={"#495E57"}
-            >
-              Menu
-            </Link>
-            <Link
-              href="#"
-              fontFamily="Karla, sans-serif"
-              fontSize="lg"
-              fontWeight={"bold"}
-              color={"#495E57"}
-            >
-              Reservation
-            </Link>
-            <Link
-              href="#"
-              fontFamily="Karla, sans-serif"
-              fontSize="lg"
-              fontWeight={"bold"}
-              color={"#495E57"}
-            >
-              Order Online
-            </Link>
-            <Link
-              href="#"
-              fontFamily="Karla, sans-serif"
-              fontSize="lg"
-              fontWeight={"bold"}
-              color={"#495E57"}
-            >
-              Login
-            </Link>
-          </HStack>
-          <IconButton
-            aria-label={isOpen ? "Close Menu" : "Open Menu"}
-            icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
-            display={{ md: "none" }} // Show only on small screens
-            onClick={onToggle}
-            colorScheme="teal"
-          />
+            <Spacer display={{ base: "none", md: "flex" }} />
 
-          <Collapse in={isOpen} animateOpacity>
-            <Stack
-              spacing={4}
-              bg="#fff"
-              p={4}
-              display={{ md: "none" }} // Hide on medium screens and up
-              position="absolute"
-              right={0}
-              left={0}
-              zIndex={10}
-            >
+            <HStack spacing={10} display={{ base: "none", md: "flex" }}>
               <Link
                 href="#"
                 fontFamily="Karla, sans-serif"
@@ -129,9 +69,76 @@ export default function Header() {
               >
                 Login
               </Link>
-            </Stack>
-          </Collapse>
-        </ul>
+            </HStack>
+
+            <IconButton
+              aria-label={isOpen ? "Close Menu" : "Open Menu"}
+              icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
+              display={{ md: "none" }}
+              onClick={onToggle}
+              colorScheme="teal"
+            />
+          </HStack>
+        </Flex>
+
+        <Collapse in={isOpen} animateOpacity>
+          <VStack
+            spacing={4}
+            bg="#fff"
+            p={4}
+            display={{ md: "none" }} // Hide on medium screens and up
+            position="absolute"
+            right={0}
+            left={0}
+            zIndex={10}
+          >
+            <Link
+              href="#"
+              fontFamily="Karla, sans-serif"
+              fontSize="lg"
+              fontWeight={"bold"}
+              color={"#495E57"}
+            >
+              Home
+            </Link>
+            <Link
+              href="#"
+              fontFamily="Karla, sans-serif"
+              fontSize="lg"
+              fontWeight={"bold"}
+              color={"#495E57"}
+            >
+              Menu
+            </Link>
+            <Link
+              href="#"
+              fontFamily="Karla, sans-serif"
+              fontSize="lg"
+              fontWeight={"bold"}
+              color={"#495E57"}
+            >
+              Reservation
+            </Link>
+            <Link
+              href="#"
+              fontFamily="Karla, sans-serif"
+              fontSize="lg"
+              fontWeight={"bold"}
+              color={"#495E57"}
+            >
+              Order Online
+            </Link>
+            <Link
+              href="#"
+              fontFamily="Karla, sans-serif"
+              fontSize="lg"
+              fontWeight={"bold"}
+              color={"#495E57"}
+            >
+              Login
+            </Link>
+          </VStack>
+        </Collapse>
       </nav>
     </Box>
   );
