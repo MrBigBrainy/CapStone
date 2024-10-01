1dcbebc77f09ed667f99190778a56372663a74ef
MrBigBrainy <mrbigbrainy@gmail.com>
2024-10-01 10:30:08 +0700

    Finish Header Section

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -7,7 +7,6 @@ import {
   VStack,
   IconButton,
   Flex,
-  Spacer,
 } from "@chakra-ui/react";
 import { HamburgerIcon, CloseIcon } from "@chakra-ui/icons";
 import { useDisclosure } from "@chakra-ui/react";
@@ -15,70 +14,72 @@ import logo from "../pictures/Logo.svg";
 
 export default function Header() {
   const { isOpen, onToggle } = useDisclosure();
+
   return (
-    <Box p={5} display={"flex"} justifyContent={"center"} as="header">
+    <Box p={5} as="header">
       <nav>
-        <Flex justify={{ base: "space-between", md: "center" }} align="center">
-          <HStack spacing={10}>
-            <Image src={logo} />
-            <Spacer display={{ base: "none", md: "flex" }} />
+        <Flex
+          justify={{ base: "space-between", md: "center" }} 
+          gap={10}
+          width="full"
+        >
+          <Image src={logo} alt="Logo" />
 
-            <HStack spacing={10} display={{ base: "none", md: "flex" }}>
-              <Link
-                href="#"
-                fontFamily="Karla, sans-serif"
-                fontSize="lg"
-                fontWeight={"bold"}
-                color={"#495E57"}
-              >
-                Home
-              </Link>
-              <Link
-                href="#"
-                fontFamily="Karla, sans-serif"
-                fontSize="lg"
-                fontWeight={"bold"}
-                color={"#495E57"}
-              >
-                Menu
-              </Link>
-              <Link
-                href="#"
-                fontFamily="Karla, sans-serif"
-                fontSize="lg"
-                fontWeight={"bold"}
-                color={"#495E57"}
-              >
-                Reservation
-              </Link>
-              <Link
-                href="#"
-                fontFamily="Karla, sans-serif"
-                fontSize="lg"
-                fontWeight={"bold"}
-                color={"#495E57"}
-              >
-                Order Online
-              </Link>
-              <Link
-                href="#"
-                fontFamily="Karla, sans-serif"
-                fontSize="lg"
-                fontWeight={"bold"}
-                color={"#495E57"}
-              >
-                Login
-              </Link>
-            </HStack>
-
-            <IconButton
-              aria-label={isOpen ? "Close Menu" : "Open Menu"}
-              icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
-              display={{ md: "none" }}
-              onClick={onToggle}
-              colorScheme="teal"
-            />
+          <HStack spacing={10} display={{ base: "none", md: "flex" }}>
+            <Link
+              href="#"
+              fontFamily="Karla, sans-serif"
+              fontSize="lg"
+              fontWeight={"bold"}
+              color={"#495E57"}
+            >
+              Home
+            </Link>
+            <Link
+              href="#"
+              fontFamily="Karla, sans-serif"
+              fontSize="lg"
+              fontWeight={"bold"}
+              color={"#495E57"}
+            >
+              Menu
+            </Link>
+            <Link
+              href="#"
+              fontFamily="Karla, sans-serif"
+              fontSize="lg"
+              fontWeight={"bold"}
+              color={"#495E57"}
+            >
+              Reservation
+            </Link>
+            <Link
+              href="#"
+              fontFamily="Karla, sans-serif"
+              fontSize="lg"
+              fontWeight={"bold"}
+              color={"#495E57"}
+            >
+              Order Online
+            </Link>
+            <Link
+              href="#"
+              fontFamily="Karla, sans-serif"
+              fontSize="lg"
+              fontWeight={"bold"}
+              color={"#495E57"}
+            >
+              Login
+            </Link>
           </HStack>
+
+          <IconButton
+            aria-label={isOpen ? "Close Menu" : "Open Menu"}
+            icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
+            display={{ md: "none" }}
+            onClick={onToggle}
+            colorScheme="teal"
+          />
         </Flex>
 
         <Collapse in={isOpen} animateOpacity>
@@ -86,11 +87,11 @@ export default function Header() {
             spacing={4}
             bg="#fff"
             p={4}
-            display={{ md: "none" }} // Hide on medium screens and up
+            display={{ base: "flex", md: "none" }}
             position="absolute"
             right={0}
             left={0}
-            zIndex={10}
+            zIndex={100}
           >
             <Link
               href="#"
