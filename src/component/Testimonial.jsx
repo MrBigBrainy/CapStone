63e91bbc5293743a46741b8a9980e04cd009d49a
MrBigBrainy <mrbigbrainy@gmail.com>
2024-10-10 18:43:47 +0700

    Finish Testimonial

--- src/component/Testimonial.jsx
+++ src/component/Testimonial.jsx
@@ -8,12 +8,20 @@ export default function Testimonial() {
         fontSize={"5xl"}
         fontFamily={"Markazi"}
         fontWeight={"semibold"}
+        mb={"20px"}
       >
         Testimonials
       </Text>
-      <Grid templateColumns="repeat(6, 1fr)">
+      <Grid templateColumns="repeat(6, 1fr)" gap={"15px"}>
         <Review name={"Jonathan Burger"} review={"This is INSANE!!!"} />
-        <Review name={"Anchari Junjareon"} review={"OMG!!! WTF! Too Delicious!"} />
+        <Review
+          name={"Anchari Junjareon"}
+          review={"OMG!!! WTF! Too Delicious!"}
+        />
+        <Review
+          name={"MyDragon ZeroUnited"}
+          review={"I will come back again!"}
+        />
       </Grid>
     </>
   );
