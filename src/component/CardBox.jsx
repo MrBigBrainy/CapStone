4bb1f28deda4251272b04c5ddecee91075dc4d8f
MrBigBrainy <mrbigbrainy@gmail.com>
2024-10-10 17:10:50 +0700

    Update Special Section

--- src/component/CardBox.jsx
+++ src/component/CardBox.jsx
@@ -1,11 +1,54 @@
-import { Card, CardBody, Text } from "@chakra-ui/react";
-
-export default function CardBox() {
+import {
+  Card,
+  CardBody,
+  CardHeader,
+  Text,
+  Heading,
+  GridItem,
+  Flex,
+  Image,
+  Icon,
+  HStack
+} from "@chakra-ui/react";
+import { TbMotorbike } from "react-icons/tb";
+export default function CardBox({headline, price, imageSource, alternateText, detail}) {
   return (
-    <Card>
-      <CardBody>
-        <Text>Summary</Text>
-      </CardBody>
-    </Card>
+    <GridItem colSpan={2}>
+      <Card borderRadius={"lg"} width={"300px"} mb={10}>
+        <Image src={imageSource} alt={alternateText} />
+        <CardHeader bg={"#EDEFEE"}>
+          <Flex justifyContent={"space-between"}>
+            <Heading size="md" fontFamily={"Karla"}>
+              {headline}
+            </Heading>
+            <Text
+              size="md"
+              fontWeight={"bold"}
+              color={"#EE9972"}
+              fontFamily={"Karla"}
+            >
+              {price}
+            </Text>
+          </Flex>
+        </CardHeader>
+        <CardBody bg={"#EDEFEE"} height={"210px"}>
+          <Text textAlign={"left"} color="#333333" fontFamily={"Karla"}>
+            {detail}
+          </Text>
+          <Flex gap={3}>
+            <Text
+              fontWeight={"bold"}
+              fontFamily={"Karla"}
+              mt={5}
+              fontSize={"lg"}
+              textAlign={"left"}
+            >
+              Order Delivery
+            </Text>
+            <Icon as={TbMotorbike} boxSize={5} alignSelf={"flex-end"} mb={1} />
+          </Flex>
+        </CardBody>
+      </Card>
+    </GridItem>
   );
 }
--- src/component/Special.jsx
+++ src/component/Special.jsx
@@ -1,26 +1,49 @@
-import { Flex, Text, VStack, Box, Button } from "@chakra-ui/react";
+import { Flex, Text, Box, Button, Grid } from "@chakra-ui/react";
 import CardBox from "./CardBox";
+import greekSaladImage from "../pictures/greekSalad.jpg";
+import bruchetta from "../pictures/bruchetta.svg";
 export default function Special () {
     return (
       <>
-        <Flex justifyContent={"center"} alignItems={"top"} p={10} mt={100}>
-          <VStack>
-            <Text
-              as={"h2"}
-              fontSize={"5xl"}
-              fontFamily={"Markazi"}
-              fontWeight={"semibold"}
-            >
-              This Weeks Specials
-            </Text>
-            <CardBox />
-          </VStack>
-          <Box w={"300px"} mt={4}>
+        <Flex
+          alignItems={"top"}
+          pt={10}
+          pb={10}
+          mt={100}
+          justifyContent={"space-between"}
+        >
+          <Text
+            as={"h2"}
+            fontSize={"5xl"}
+            fontFamily={"Markazi"}
+            fontWeight={"semibold"}
+          >
+            This Weeks Specials!
+          </Text>
+          <Box mt={4}>
             <Button as="button" bg="#F4CE1C" rounded={"5"} fontWeight={"bold"}>
               Online Menu
             </Button>
           </Box>
         </Flex>
+        <Grid templateColumns="repeat(6, 1fr)">
+          <CardBox
+            headline="Greek Salad"
+            price="$12.99"
+            imageSource={greekSaladImage}
+            alternateText="Greek Salad"
+            detail="The famous greek salad of crispy lettuce, peppers, olives, and our Chicago style feta cheese, garnished with crunchy garlic and rosementary croustons"
+          
+          />
+          <CardBox
+            headline="Bruchetta"
+            price="$5.99"
+            imageSource={bruchetta}
+            alternateText="Bruchetta"
+            detail="Our Bruschetta is made from grilled bread that has been smeared with garlic and seasoned with salt and olive oil. "
+           
+          />
+        </Grid>
       </>
     );
 }
