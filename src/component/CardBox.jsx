92c2b837cd93be976828f075ed897857c7e30a12
MrBigBrainy <mrbigbrainy@gmail.com>
2024-10-12 15:44:18 +0700

    Update Card

--- src/component/CardBox.jsx
+++ src/component/CardBox.jsx
@@ -9,7 +9,7 @@ import {
   Flex,
   Image,
   Icon,
-  HStack
+  Box,
 } from "@chakra-ui/react";
 import { TbMotorbike } from "react-icons/tb";
 export default function CardBox({headline, price, imageSource, alternateText, detail}) {
@@ -29,42 +29,49 @@ export default function CardBox({headline, price, imageSource, alternateText, de
           height={"200px"}
           borderRadius={"lg"}
         />
-        <CardHeader bg={"#EDEFEE"}>
-          <Flex justifyContent={"space-between"}>
-            <Heading size="md" fontFamily={"Karla"}>
-              {headline}
-            </Heading>
-            <Text
-              size="md"
-              fontWeight={"bold"}
-              color={"#EE9972"}
-              fontFamily={"Karla"}
-            >
-              {price}
+        <Box>
+          <CardHeader bg={"#EDEFEE"}>
+            <Flex justifyContent={"space-between"}>
+              <Heading size="md" fontFamily={"Karla"}>
+                {headline}
+              </Heading>
+              <Text
+                size="md"
+                fontWeight={"bold"}
+                color={"#EE9972"}
+                fontFamily={"Karla"}
+              >
+                {price}
+              </Text>
+            </Flex>
+          </CardHeader>
+          <CardBody bg={"#EDEFEE"}>
+            <Text textAlign={"left"} color="#333333" fontFamily={"Karla"}>
+              {detail}
             </Text>
-          </Flex>
-        </CardHeader>
-        <CardBody bg={"#EDEFEE"}>
-          <Text textAlign={"left"} color="#333333" fontFamily={"Karla"}>
-            {detail}
-          </Text>
-        </CardBody>
+          </CardBody>
 
-        <CardFooter bg={"#EDEFEE"}>
-          <Flex gap={3}>
-            <Text
-              fontWeight={"bold"}
-              fontFamily={"Karla"}
-              mt={5}
-              fontSize={"lg"}
-              textAlign={"left"}
-              alignSelf={"flex-end"}
-            >
-              Order Delivery
-            </Text>
-            <Icon as={TbMotorbike} boxSize={5} alignSelf={"flex-end"} mb={1} />
-          </Flex>
-        </CardFooter>
+          <CardFooter bg={"#EDEFEE"}>
+            <Flex gap={3}>
+              <Text
+                fontWeight={"bold"}
+                fontFamily={"Karla"}
+                mt={5}
+                fontSize={"lg"}
+                textAlign={"left"}
+                alignSelf={"flex-end"}
+              >
+                Order Delivery
+              </Text>
+              <Icon
+                as={TbMotorbike}
+                boxSize={5}
+                alignSelf={"flex-end"}
+                mb={1}
+              />
+            </Flex>
+          </CardFooter>
+        </Box>
       </Card>
     </GridItem>
   );
