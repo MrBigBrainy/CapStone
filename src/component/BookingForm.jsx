c4f98169ad5f3b30a7c85310738462313d06ded5
MrBigBrainy <mrbigbrainy@gmail.com>
2024-10-17 13:43:41 +0700

    Start Form

--- src/component/BookingForm.jsx
+++ src/component/BookingForm.jsx
@@ -1,17 +1,67 @@
-import { FormControl, FormLabel, Input, Button } from "@chakra-ui/react";
+import { FormControl, FormLabel, Input, Button, Select, Flex} from "@chakra-ui/react";
+import { useState } from "react";
 export default function BookingForm () {
+    const [formData, setFormData] = useState({
+        date: '',
+         time: '',
+        numberOfGuests: '',
+        occasion: '',
+    })
     function handleSubmit (event) {
         event.preventDefault();
         console.log("Submitted");
     }
+
+    function handleChange(event, objectName){
+        const {value} = event.target;
+        setFormData((prevState)=> ({
+            ...prevState,
+            [objectName]: value,
+        })) 
+    }
     return (
       <>
-        <FormControl m ={10}>
-          <FormLabel textAlign={"center"}>
-            Number of guests: <Input type="number" name="guests" />
-          </FormLabel>
-          <br />
-          <Button
+        <Flex justifyContent={"center"}>
+          <FormControl
+            m={10}
+            style={{ display: "grid", maxWidth: "200px", gap: "20px" }}
+          >
+            <FormLabel htmlFor="res-date"> Choose Date </FormLabel>
+            <Input type="date" id="res-date" onChange={(event) => handleChange(event, "date")} />
+            <FormLabel htmlFor="res-time">Choose time</FormLabel>
+            <Select id="res-time ">
+              <option>17:00</option>
+              <option>18:00</option>
+              <option>19:00</option>
+              <option>20:00</option>
+              <option>21:00</option>
+              <option>22:00</option>
+            </Select>
+            <FormLabel htmlFor="guests">Number of guests</FormLabel>
+            <Input type="number" placeholder="1" min="1" max="10" id="guests" />
+            <FormLabel htmlFor="occasion">Occasion</FormLabel>
+            <Select id="occasion">
+              <option>Birthday</option>
+              <option>Anniversary</option>
+            </Select>
+
+            <Input
+              type="submit"
+              value="Make Your reservation"
+              bg={"#495E57"}
+              color={"white"}
+              onClick={handleSubmit}
+              cursor={"pointer"}
+            />
+          </FormControl>
+        </Flex>
+      </>
+    ); 
+}
+    
+
+/*
+ <Button
             type="Submit"
             onClick={handleSubmit}
             bg={"#495E57"}
@@ -20,8 +70,5 @@ export default function BookingForm () {
           >
             Submit
           </Button>
-        </FormControl>
-      </>
-    ); 
-}
-    
+
+   */
